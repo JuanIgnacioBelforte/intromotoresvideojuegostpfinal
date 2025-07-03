2d8751e9ca5cae99aee19a450b9609c06330fcde
NamuKimeraVT <namuquimera@gmail.com>
2025-07-03 20:42:53 -0300

    Fisicas del jugador

--- scripts/aventurero.gd
+++ scripts/aventurero.gd
@@ -1,55 +1,51 @@
 extends CharacterBody2D
+class_name Player
+
 
 # Variables
-
-var speed = 200 # Velocidad de movimiento
-var jump_height = -400 # Fuerza del salto
-var gravity = 800 # Gravedad
-
-var velocity = Vector2.ZERO # Velocidad actual
+@export var speed = 200 # Velocidad de movimiento
+@export var jump_height = -500 # Fuerza del salto
+@export var gravity = 800 # Gravedad
 
 # Señales
 
-#onready var animatedSprite = $AnimatedSprite # Referencia al nodo AnimatedSprite
+@onready var animatedSprite = $AnimatedSprite # Referencia al nodo AnimatedSprite
+
 
 func _physics_process(delta):
 	# Movimiento horizontal
 	velocity.x = 0
-	
-if Input.is_action_pressed("ui_right"):
+
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
-		$AnimatedSprite.flip_h = false # Voltear sprite si es necesario
-		$AnimatedSprite.play("run") # Animación de correr
-	
-	
-if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.flip_h = false # Voltear sprite si es necesario
+		$AnimatedSprite2D.play("run") # Animación de correr
+
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= speed
-		$AnimatedSprite.flip_h = true # Voltear sprite si es necesario
-		$AnimatedSprite.play("run") # Animación de correr
-	
-# Salto
-	
-if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		$AnimatedSprite2D.flip_h = true # Voltear sprite si es necesario
+		$AnimatedSprite2D.play("run") # Animación de correr
+
+	# Salto
+
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_height
-		$AnimatedSprite.play("jump") # Animación de salto
-	
-# Aplicar gravedad
+		$AnimatedSprite2D.play("jump") # Animación de salto
+
+	# Aplicar gravedad
 	velocity.y += gravity * delta
-	
-# Movimiento
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
-# Ataque
-	
-if Input.is_action_just_pressed("ui_attack"):
-		$AnimatedSprite.play("attack") # Animación de ataque
+
+	# Movimiento
+	self.velocity = velocity
+	move_and_slide()
+
+	# Ataque
+
+	if Input.is_action_just_pressed("ui_attack"):
+		$AnimatedSprite2D.play("attack") # Animación de ataque
 		# Aquí puedes agregar el código para detectar colisiones y aplicar daño
 
-# Animación idle
+	# Animación idle
 
-if velocity.x == 0 and is_on_floor():
-		$AnimatedSprite.play("idle") # Animación idle
-
-
-func _on_animaciones_sprite_frames_changed() -> void:
-	pass _physics_process(delta)
+	if velocity.x == 0 and is_on_floor():
+		$AnimatedSprite2D.play("idle") # Animación idle
