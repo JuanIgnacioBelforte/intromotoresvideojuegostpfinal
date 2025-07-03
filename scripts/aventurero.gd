2dc1baf4ab48783ae209b9c7f28cb01be93e924d
NamuKimeraVT <namuquimera@gmail.com>
2025-07-03 19:34:43 -0300

    Inicio del desarrollo

---
+++ scripts/aventurero.gd
@@ -0,0 +1,52 @@
+extends AnimatedSprite2D
+
+# Variables
+
+var speed = 200 # Velocidad de movimiento
+var jump_height = -400 # Fuerza del salto
+var gravity = 800 # Gravedad
+
+var velocity = Vector2.ZERO # Velocidad actual
+
+# Señales
+
+#onready var animatedSprite = $AnimatedSprite # Referencia al nodo AnimatedSprite
+
+func _physics_process(delta):
+	# Movimiento horizontal
+	velocity.x = 0
+	
+if Input.is_action_pressed("ui_right"):
+		velocity.x += speed
+		#$AnimatedSprite.flip_h = false # Voltear sprite si es necesario
+		$AnimatedSprite.play("run") # Animación de correr
+		
+	
+	
+if Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+		#$AnimatedSprite.flip_h = true # Voltear sprite si es necesario
+		$AnimatedSprite.play("run") # Animación de correr
+	
+	# Salto
+	
+if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_height
+		$AnimatedSprite.play("jump") # Animación de salto
+	
+	# Aplicar gravedad
+	velocity.y += gravity * delta
+	
+	# Movimiento
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	# Ataque
+	
+if Input.is_action_just_pressed("ui_attack"):
+		$AnimatedSprite.play("attack") # Animación de ataque
+		# Aquí puedes agregar el código para detectar colisiones y aplicar daño
+	
+	# Animación idle
+	
+if velocity.x == 0 and is_on_floor():
+		$AnimatedSprite.play("idle") # Animación idle
