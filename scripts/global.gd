9342d0082a3f7ef489dcad039417ac36955ab722
JuanIgnacioBelforte <juanibel99@gmail.com>
2026-01-17 06:26:39 -0300

    Revert "Ediciones"
    This reverts commit 8fbd4395d2727a017d5189f324eeca6dc4d908da.

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,8 +1,3 @@
 extends Node
 
 var score: int
-var axis : Vector2
-
-func get_axis() -> Vector2:
-	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	return axis.normalized()
